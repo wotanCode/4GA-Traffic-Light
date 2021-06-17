f3a5d0bc722d8704b86d3377ee9eab2a821d0a30
Pedro Yanez <pedro_y91@outlook.com>
2021-06-17 23:27:33 +0000

    finalizado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+//include images into your bundle
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("green");
 	return (
-		<div className="text-center mt-5">
-            <h1>prueba</h1>
+		<div className="text-center">
+			<div className="soporte"></div>
+			<div className="semaforo">
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"red" + (color === "red" ? " iluminado" : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"yellow" + (color === "yellow" ? " iluminado" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"green" + (color === "green" ? " iluminado" : "")
+					}></div>
+			</div>
 		</div>
 	);
 }
